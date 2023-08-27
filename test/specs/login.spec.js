5d28b55033063a25c5ce0b8477952a6a821a7e6b
vzp777 <victorzamanapenna@gmail.com>
2023-08-27 19:21:01 -0300

    Teste mobile part1

---
+++ test/specs/login.spec.js
@@ -0,0 +1,12 @@
+describe('Access Admin Panel', () => {
+  it('should login with valid credentials', async () => {
+    setTimeout (() =>{
+    console.log('abriu')
+
+
+    }, 50000)
+   
+  });
+});
+
+
